8227d032893f4bc108b32267a8c79bed206a602a
Paul Gichuki <pherndegz@gmail.com>
2021-09-30 06:42:14 +0300

    feat: setup movielist on the Home screen

---
+++ components/MovieCard.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Image, Text, View, StyleSheet } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+
+const MovieCard = ({ movie, index } : {movie: any, index: any}) => {
+    return (
+            <View style={index === 0 ? styles.topMovie : styles.movieCard}>
+                <Image
+                    source={{
+                        uri: `https://image.tmdb.org/t/p/w500` + movie.poster_path
+                    }}
+                    style={styles.poster}
+                />
+                <View style={styles.movieDetail}>
+                    {index === 0 ? <Text style={styles.topMovieTag}>🥇 Top movie this week</Text> : null}
+                    <Text style={styles.movieTitle}>{movie.original_title}</Text>
+                    <Text style={styles.movieExtraDetails}>Fiction / Fantasy</Text>
+                    <Text style={styles.movieExtraDetails}>2021</Text>
+                </View>
+            </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    movieCard: {
+        flex: 1,
+        flexDirection: "row",
+        minWidth: '95%',
+        margin: 'auto',
+        marginVertical: 10,
+        backgroundColor: '#252634',
+        borderRadius: 15
+    },
+    topMovie: {
+        flexDirection: "row",
+        minWidth: '95%',
+        margin: 'auto',
+        marginVertical: 10,
+        backgroundColor: '#007CFF',
+        borderRadius: 15
+    },
+    topMovieTag: {
+        color: '#fff',
+        fontSize: 17,
+        marginBottom: 15,
+        fontWeight: '700'
+
+    },
+    poster: {
+        width: 150,
+        height: 195,
+        borderTopLeftRadius: 15,
+        borderBottomLeftRadius: 15,
+    },
+    movieDetail: {
+        flexDirection: 'column',
+        width: 0,
+        flexGrow: 1,
+        paddingVertical: 30,
+        paddingHorizontal: 15
+    },
+    movieTitle: {
+        color: '#fff',
+        fontSize: 19,
+        fontWeight: 'bold',
+        lineHeight: 20,
+        marginBottom: 20
+    },
+    movieExtraDetails: {
+        color: '#CDCED1',
+        fontSize: 13,
+        fontWeight: 'bold',
+        lineHeight: 20
+    }
+})
+
+export default MovieCard;
--- screens/TopMoviesScreen.tsx
+++ screens/TopMoviesScreen.tsx
@@ -1,16 +1,50 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
-import EditScreenInfo from '../components/EditScreenInfo';
+import MovieCard from '../components/MovieCard';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TopMoviesScreen({ navigation }: RootTabScreenProps<'TopMoviesScreen'>) {
+
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getMovies();
+  })
+
+  const getMovies = () => {
+    setLoading(true)
+    axios.get('https://api.themoviedb.org/3/trending/movie/week?api_key=d4c38aaf3b6b6bf2e1f7a5418a14e582')
+      .then(response => {
+        setLoading(false)
+        setMovies(response.data.results)
+      })
+      .catch(error => {
+        setLoading(false)
+        console.log(error)
+      })
+  }
+
+  const mapMovies = (movies: []) => {
+    return movies.map((movie: {}, index: number) => {
+      return (
+        <TouchableWithoutFeedback key={index} onPress={() => { navigation.navigate("MovieDetails", { selectedMovie: movie, movies: movies }) }}>
+          <MovieCard movie={movie} index={index} />
+        </TouchableWithoutFeedback>
+      )
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text style={styles.title}>Top Movies</Text>
+      <ScrollView style={styles.movieList}>
+        {mapMovies(movies)}
+      </ScrollView>
     </View>
   );
 }
@@ -18,16 +52,22 @@ export default function TopMoviesScreen({ navigation }: RootTabScreenProps<'TopM
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 20,
+    backgroundColor: '#1B1C2A'
   },
   title: {
+    color: '#fff',
     fontSize: 20,
     fontWeight: 'bold',
+    marginBottom: 15,
+    marginLeft: 20,
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
   },
+  movieList: {
+    paddingHorizontal: 10
+  }
 });
